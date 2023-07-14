c71e89cdcf96423f3e49c7d8f3566b22ac62ef01
PilBel <pillebeldman@gmail.com>
2023-07-14 13:22:11 +0300

    Bonus Create Deletion Tests

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -7,8 +7,8 @@ describe('Issue delete', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-    //open issue detail modal with title from line 16  
-    cy.contains(issueTitle).click();
+      //open issue detail modal with title from line 16  
+      cy.contains(issueTitle).click();
     });
   });
 
@@ -17,10 +17,19 @@ describe('Issue delete', () => {
 
   it('Should delete issue successfully', () => {
     //add steps to delete issue
-    
+    IssueModal.clickDeleteButton();
+    IssueModal.confirmDeletion();
+    cy.wait(5000);
+    IssueModal.ensureIssueIsNotVisibleOnBoard(issueTitle);
+
   });
 
   it('Should cancel deletion process successfully', () => {
-    //add steps to start deletion proces but cancel it
+    //add steps to start deletion process but cancel it
+    IssueModal.clickDeleteButton();
+    IssueModal.cancelDeletion();
+    IssueModal.closeDetailModal();
+    cy.wait(5000);
+    IssueModal.ensureIssueIsVisibleOnBoard(issueTitle);
   });
 });
